6ed7b22ce47e9d26e28ae4134753ccf00eaf8f9d
jorgeProgramaEn5 <116350790+jorgeProgramaEn5@users.noreply.github.com>
2023-05-21 12:45:03 -0400

    add robot_bici.png

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from '../style';
-import { discount, robot } from '../assets';
+import { discount, robot, robot_bici, } from '../assets';
 import GetStarted from './GetStarted';
 
 const Hero = () => (
@@ -36,7 +36,7 @@ const Hero = () => (
       </p>
     </div>
     <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-      <img src={robot} alt="robot" className='w-[100%] h-[100%] relative z-[5]' />
+      <img src={ robot } alt="robot" className='w-[100%] h-[100%] relative z-[5]' />
       <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'></div>
       <div className='absolute z-[1] w-[80%] h-[80%] bottom-40 white__gradient  rounded-full'></div>
       <div className='absolute z-[1] w-[50%] h-[50%] right-20 bottom-20 blue__gradient'></div>
